f856dce3221b13cb62c876b42913820b72682b08
eightalex <eight.alex@gmail.com>
2016-11-22 15:46:24 +0200

    Сверстал вывод результатов

---
+++ app/js/toggle-table-checkbox.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	var table = document.getElementsByClassName('result')[0];
+	table.onclick = function(event) {
+		var target = event.target;
+		var className;
+		while (target != table) {
+			className = target.className.split(' ')[0];
+			if (className === 'result__row') {
+				var checkbox = target.querySelectorAll('input')[0];
+				checkbox.checked = !checkbox.checked;
+				target.classList.toggle('result__row--selected');
+				return;
+			}
+			target = target.parentNode;
+		}
+	}
+}
